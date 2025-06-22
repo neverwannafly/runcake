067102ff65c5fd710ab3124f261d59a4817551dc
Shubham Anand <shubham@interviewbit.com>
2025-06-22 19:07:59 +0530

    add allowed hosts

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
+const allowedHosts = import.meta.env.VITE_ALLOWED_HOSTS || 'localhost,127.0.0.1'
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    allowedHosts: allowedHosts.split(','),
+  },
 })
